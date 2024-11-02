f73cf6f13435ea97eb5a3f57f647d82e2b248036
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-11-02 18:52:46 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json({limit:'10mb'}));
 const port = 3000;
 
 app.use(cors({
-    origin: 'https://course-selling-hjzf.vercel.app/', 
+    origin: 'https://course-selling-hjzf.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
   }));
